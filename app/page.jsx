41a9763f8fd2d14d9d128c620b012273bef6f798
Salman Hasan <s.hasan@unguarded.com>
2023-12-16 17:04:42 -0500

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold.mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo site for the traversey Media Next.js & Clerk tutorial. Go
+        ahead and sign up or sign in!
+      </p>
+    </>
+  );
+}
